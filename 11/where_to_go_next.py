7b305f52b77d4a8c0ab422ab8dd504b9e6c0e108
p4v4n <pavan.barcelona@gmail.com>
2017-04-24 16:08:15 +0530

    added ch-11(where to go next)

---
+++ 11/where_to_go_next.py
@@ -0,0 +1,31 @@
+# Where to go next4
+
+"""
+Trees:
+Binary Search Trees,B-trees,Red-black trees,Heaps,Splay trees
+
+Inverted Indexes:
+(uses hash map)
+
+The Fourier transform:
+
+Parallel algorithms:
+
+MapReduce:
+(distributed algorithm)
+
+Bloom filters:
+
+HyperLogLog:
+
+The SHA algorithms:
+(hash function)
+
+Locality-sensitive hashing:
+(Simhash)
+
+Diffie-Hellman key exchange and RSA:
+
+Linear Programming:
+(Simplex algorithm)
+"""
