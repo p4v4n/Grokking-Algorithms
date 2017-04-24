8ab47f58d8a29f466bb144884b3968dbae611961
p4v4n <pavan.barcelona@gmail.com>
2017-04-24 11:56:04 +0530

    added ch-10(K-nearest neighbours)

---
+++ 10/k-nearest_neighbours.py
@@ -0,0 +1,19 @@
+# K-Nearest Neighbours
+
+"""
+
+• KNN is used for classification and regression and involves looking
+at the k-nearest neighbors.
+• Classification = categorization into a group.
+• Regression = predicting a response (like a number).
+• Feature extraction means converting an item (like a fruit or a user)
+into a list of numbers that can be compared.
+• Picking good features is an important part of a successful KNN
+algorithm.
+
+Introduction to Machine Learning:
+ex:
+1) OCR(optical character recognition)
+2) Building a Spam Filter - uses a Naive Bayes Classifies Algorithm
+
+"""
